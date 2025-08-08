94593395c36fb6a4856ab9e682fe2426ba25073d
BenZeta <benpardede.hacktiv8@gmail.com>
2025-08-08 16:22:18 +0700

    fix: change ecosystem config env to prod

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'vite-frontend',
-      script: 'npm',
-      args: 'run dev',
-      interpreter: 'node', // or leave it default (usually works)
-      cwd: '/root/outmanage-frontend',
+      name: "vite-frontend",
+      script: "npm",
+      args: "run dev",
+      interpreter: "node", // or leave it default (usually works)
+      cwd: "/root/outmanage-frontend",
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: "production",
       },
     },
   ],
